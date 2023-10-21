6b10f29fcddb5df27a66a78d85dac7d166dca4ef
Emiliano <emilianomachado967@gmail.com>
2023-10-20 21:56:50 -0500

    first commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { useTranslations } from 'next-intl';
 import bg from '@/public/bg.webp'
+import bgMobile from '@/public/bg-mobile.jpg'
 
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
@@ -30,30 +31,41 @@ export default function Hero() {
 
   return (
     <>
-      <div className="relative overflow-hidden min-h-[900px]">
-        <Image 
-          src={bg} 
-          alt={''} 
-          layout="responsive" 
+     <div className="relative overflow-hidden min-h-screen sm:min-h-[900px]">
+      <div className="hidden md:block">
+        <Image
+          src={bg}
+          alt=""
+          layout="fixed"
           priority
-          className="absolute inset-0 object-cover w-full h-full" 
+          className="absolute inset-0 object-cover w-full h-full"
         />
-        <div className="absolute inset-0 flex items-center justify-center py-8">
-          <div className="space-y-5 bg-transparent w-full p-4 text-center text-white">
-            <h2 className="text-2xl sm:text-4xl lg:text-6xl font-bold uppercase">
-              {t('title')}
-            </h2>
-            <p className="text-sm sm:text-base md:text-lg lg:text-xl">
-              {t('description')}
-            </p>
-            <Link href="#AboutUs" className="btn btn-primary" onClick={(event) => handleClick(event, "AboutUs")}>
-              <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-md text-sm sm:text-base md:text-lg lg:text-xl">
-                {t('button')}
-              </button>
-            </Link>
-          </div>
+      </div>
+      <div className="md:hidden">
+        <Image
+          src={bgMobile}
+          alt=""
+          layout="fixed"
+          priority
+          className="absolute inset-0 object-cover w-full h-full"
+        />
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center py-4 sm:py-8">
+        <div className="w-full max-w-screen-lg mx-4 sm:mx-auto text-center text-white p-4">
+          <h2 className="text-lg sm:text-2xl md:text-3xl lg:text-6xl font-bold uppercase">
+            {t('title')}
+          </h2>
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl">
+            {t('description')}
+          </p>
+          <Link href="#AboutUs" className="btn btn-primary" onClick={(event) => handleClick(event, "AboutUs")}>
+            <button className="bg-blue-500 text-white px-2 sm:px-4 py-2 mt-2 sm:mt-4 rounded-md text-sm sm:text-base md:text-lg lg:text-xl">
+              {t('button')}
+            </button>
+          </Link>
         </div>
       </div>
+    </div>
     </>
   );
 }
